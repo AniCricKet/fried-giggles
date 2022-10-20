ee9a32b9873151574957a894c555c252aae7909f
Soham360 <sohamkamat10@gmail.com>
2022-10-20 15:43:46 -0700

    fix

--- words.py
+++ words.py
@@ -1,16 +1,8 @@
 wordlist = {
-<<<<<<< HEAD
-    'aniket': '- Chakradeo',
-    'soham': '- Kamat',
-    "ryan": "- hakimipour",
-    "lucas": "- moore",
-    'david': '- Warner'
-=======
     'aniket': 'Chakradeo',
     'soham': 'Kamat',
     "ryan": "hakimipour",
     "lucas": "moore",
     'david': 'Warner',
     "virat": "kohli",
->>>>>>> 67dfb45e73c1361586261a10d16ef39f85db4c5a
 }
