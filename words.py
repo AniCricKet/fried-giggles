d1f4453ceff6b3a897e62ae25ea495fd5cfdd6f6
Aniket Chakradeo <aniket@chakradeo.net>
2022-10-21 14:47:35 -0700

    initial words added

--- words.py
+++ words.py
@@ -1,8 +1,11 @@
 wordlist = {
-    'aniket': 'Chakradeo',
-    'soham': 'Kamat',
-    "ryan": "hakimipour",
-    "lucas": "moore",
-    'david': 'Warner',
-    "virat": "kohli",
+    'Master': 'A person who has other people working for him',
+    'Developer': 'A person who develops',
+    'Addition': 'The action of adding a value to another value',
+    'Subtraction': 'The action of removing one number from another number or value',
+    'Division': 'The action of separating something into smaller parts',
+    'Multiplication': 'The action of multiplying',
+    'Lyntax': 'Our team name',
+    'Language': 'A system of notation for writing computer programs',
+    'Syntax':  'Rules that define the structure of a language',
 }
