f24eb6b96437e7d81507dd90093314e8ae0934a0
RyanHaki <hakimipourryan@gmail.com>
2022-10-25 15:28:14 -0700

    Delete

--- words.py
+++ words.py
@@ -18,69 +18,35 @@ wordlist = {
     'integer': 'A whole number',
     'boolean': 'A variable which can only be either true or false',
     'conditional': 'Commands which perform different actions depending on weather a boolean condition evaluates the statement to be true or false',
-<<<<<<< HEAD
-<<<<<<< HEAD
     'function': 'Self contained modules of code that accomplish a specific task',
     'directory': 'A file system cataloging structure which contains references to other computer files',
     'variable': 'A value that can change, depending on conditions or on information passed to the program',
     'sprites': 'A two-dimensional image or animated image that plays a specific role, often independently manipulated, within a larger image environment.',
-<<<<<<< HEAD
-<<<<<<< HEAD
     'jupyter': 'A free, open-source, interactive web tool known as a computational notebook, which researchers can use to combine software code, computational output, explanatory text and multimedia resources in a single document.',
     'markdown': 'A markup language that formats plain text',
-<<<<<<< HEAD
-<<<<<<< HEAD
     'binary': 'A system where numbers and values are expressed 0 or 1',
     'array': 'A data structure consisting of a collection of elements (values or variables), each identified by at least one array index or key.',
-<<<<<<< HEAD
-<<<<<<< HEAD
     'dictionary': 'An abstract data type that defines an unordered collection of data as a set of key-value pairs.',
-<<<<<<< HEAD
-<<<<<<< HEAD
     'list': 'An abstract data type that represents a finite number of ordered values, where the same value may occur more than once',
-=======
     'list': 'An abstract data type that represents a finite number of ordered values, where the same value may occur more than once.',
->>>>>>> 93ad7b0 (Canadia)
     'argument': 'The actual input expression passed/supplied to a function, procedure, or routine in the invocation/call statement', 
     'input': 'Data that is entered into or received by a computer',
-=======
     'function': 'self contained modules of code that accomplish a specific task',
-=======
-    'function': 'Self contained modules of code that accomplish a specific task',
->>>>>>> d698c18 (lucas)
     'directory': 'A file system cataloging structure which contains references to other computer files',
     'variable': 'A value that can change, depending on conditions or on information passed to the program',
     'sprites': 'a two-dimensional image or animated image that plays a specific role, often independently manipulated, within a larger image environment.',
-=======
->>>>>>> f261385 (Canadia)
     'jupyter': 'a free, open-source, interactive web tool known as a computational notebook, which researchers can use to combine software code, computational output, explanatory text and multimedia resources in a single document.',
-=======
     'jupyter': 'A free, open-source, interactive web tool known as a computational notebook, which researchers can use to combine software code, computational output, explanatory text and multimedia resources in a single document.',
->>>>>>> 276a423 (Canadia)
     'markdown': 'a markup language that formats plain text',
-=======
->>>>>>> f204584 (Canadia)
     'binary': 'a system where numbers and values are expressed 0 or 1',
-=======
     'binary': 'A system where numbers and values are expressed 0 or 1',
->>>>>>> b90708a (Canadia)
     'array': 'a data structure consisting of a collection of elements (values or variables), each identified by at least one array index or key.',
-=======
->>>>>>> 490ac22 (Canadia)
     'dictionary': 'an abstract data type that defines an unordered collection of data as a set of key-value pairs.',
-=======
     'dictionary': 'An abstract data type that defines an unordered collection of data as a set of key-value pairs.',
->>>>>>> a187152 (Canadia)
     'list': 'an abstract data type that represents a finite number of ordered values, where the same value may occur more than once.',
-=======
     'list': 'An abstract data type that represents a finite number of ordered values, where the same value may occur more than once.',
->>>>>>> 4e9366a (Canadia)
     'argument': 'the actual input expression passed/supplied to a function, procedure, or routine in the invocation/call statement', 
-<<<<<<< HEAD
     'input': 'data that is entered into or received by a computer',
->>>>>>> 150ce9d (message)
-=======
     'input': 'Data that is entered into or received by a computer',
->>>>>>> 957ee35 (message)
     'output': 'Any information processed by and sent out from a computer or other electronic device',
 }
