06d3616fdece962f6085700044d104f15f2d72f3
RyanHaki <hakimipourryan@gmail.com>
2022-10-25 14:37:52 -0700

    message

--- words.py
+++ words.py
@@ -34,7 +34,7 @@ wordlist = {
 =======
     'function': 'self contained modules of code that accomplish a specific task',
     'directory': 'A file system cataloging structure which contains references to other computer files',
-    'variable': 'a value that can change, depending on conditions or on information passed to the program',
+    'variable': 'A value that can change, depending on conditions or on information passed to the program',
     'sprites': 'a two-dimensional image or animated image that plays a specific role, often independently manipulated, within a larger image environment.',
     'jupyter': 'a free, open-source, interactive web tool known as a computational notebook, which researchers can use to combine software code, computational output, explanatory text and multimedia resources in a single document.',
     'markdown': 'a markup language that formats plain text',
