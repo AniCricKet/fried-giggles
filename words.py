5bfa699c9530dfa9ff3565046f4276f3ef4375c7
Soham360 <sohamkamat10@gmail.com>
2022-11-03 14:40:42 -0700

    words update

--- words.py
+++ words.py
@@ -1,10 +1,6 @@
 wordlist = {
     'scrummaster': 'A facilitator in an agile development team',
     'developer': 'A person who develops',
-    'addition': 'The action of adding a value to another value',
-    'subtraction': 'The action of removing one number from another number or value',
-    'division': 'The action of separating something into smaller parts',
-    'multiplication': 'The action of multiplying',
     'lyntax': 'Our team name',
     'language': 'A system of notation for writing computer programs',
     'syntax':  'Rules that define the structure of a language',
